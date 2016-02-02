41bbd732d5a419ed6e866e5e24138cf67e041d32
jcarltonj <jcarltonj@gmail.com>
2016-02-02 11:34:44 -0600

    Initial Commit

---
+++ Sample table navigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sample table navigation
+//
+//  Created by Carlton Jester on 2/2/16.
+//  Copyright © 2016 Carlton Jester. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
